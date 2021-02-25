874d608cbedaca502e07ccbac36584d6d7618f76
wholelake <julyfire@yeah.net>
2021-02-25 11:17:04 +0800

    配置

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
     header("Content-type:text/html;charset=utf-8");
     date_default_timezone_set('Asia/Shanghai');
-    
-    define('RUN_ENV', isset($_SERVER['RUN_ENV']) ? $_SERVER['RUN_ENV'] : 'dev');
-
-    require_once('./config/'.RUN_ENV.'.php');
+    require_once('./config.php');
     require_once('./framework/main.php');
     MAIN::run($config);
