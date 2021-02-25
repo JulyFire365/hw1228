b7be3dfd1328dcc7c000dae94f943536e0e4bc89
wholelake <julyfire@yeah.net>
2021-02-25 10:21:52 +0800

    配置环境常量

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
     header("Content-type:text/html;charset=utf-8");
     date_default_timezone_set('Asia/Shanghai');
-    require_once('./config/pro.php');
+    
+    define('RUN_ENV', isset($_SERVER['RUN_ENV']) ? $_SERVER['RUN_ENV'] : 'dev');
+
+    require_once('./config/'.RUN_ENV.'.php');
     require_once('./framework/main.php');
     MAIN::run($config);
