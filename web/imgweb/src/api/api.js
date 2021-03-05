aeeb98f22c9a7054f07d5890ee947cb802a95ac2
frank <modestfrank@qq.com>
2021-03-05 18:39:32 +0800

    列表接口 换了

--- web/imgweb/src/api/api.js
+++ web/imgweb/src/api/api.js
@@ -10,7 +10,7 @@ export function getPhoto(id) {
 
 export function getAlbum() {
   return request({
-    url: '/index.php?controller=folder&method=getAllFile',
+    url: '/index.php?controller=folder&method=getList',
     method: 'get'
   })
 }
