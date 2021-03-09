eaa942c61eb905f969d1fd0d564741ae39cd701d
wholelake <julyfire@yeah.net>
2021-03-09 15:06:24 +0800

    优化

--- frontend/src/components/upload.vue
+++ frontend/src/components/upload.vue
@@ -65,6 +65,17 @@
     </div>
 </template>
 <script>
+
+Array.from(document.getElementsByClassName('el-select')).forEach((item) => {
+      item.children[0].children[0].removeAttribute('readOnly')
+      item.children[0].children[0].onblur = function () {
+        let _this = this
+        setTimeout(() => {
+          _this.removeAttribute('readOnly')
+        }, 200)
+      }
+    });
+    
 import { uploadStatic, foldRecordInDb } from '@/request'
 export default {
     name: 'upload',
